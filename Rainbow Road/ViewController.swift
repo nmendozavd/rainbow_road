0a95d6ba960daff38d20009c47ecdc31e3068645
Noel Mendoza <nmendozavd@gmail.com>
2018-01-16 18:16:30 -0800

    Initial Commit

---
+++ Rainbow Road/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rainbow Road
+//
+//  Created by Noel Mendoza on 1/16/18.
+//  Copyright © 2018 Noel Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
